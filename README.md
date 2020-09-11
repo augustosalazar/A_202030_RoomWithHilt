60445ae8139558163ca95ff666979e38b6476d97
augustosalazar <augustosalazar@uninorte.edu.co>
2020-09-11 14:46:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejemplo de un aplicación sencilla de Room con Hilt- 202030
+
+Basado en "Dagger-Hilt Setup - MVVM Running Tracker App"
---
+++ app/src/main/java/com/uninorte/a_202030_roomwithhilt/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.uninorte.a_202030_roomwithhilt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment(R.layout.fragment_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+    }
+}
